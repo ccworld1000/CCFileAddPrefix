4b6dd8739dfe8d27e776d1ee98683900b7b0e59a
cc <ccworld1000@gmail.com>
2017-06-03 19:12:02 +0800

    add CCFileAddPrefix

---
+++ CCFileAddPrefix.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+#//
+#//  Created by CC on 03/06/2017.
+#//  Copyright © 2017 CC | ccworld1000@gmail.com . All rights reserved.
+#//
+
+use v5.10;
+use POSIX;
+use File::Copy;
+
+say "add prefix ...";
+print "[Enter something, Set your Prefix]:";
+chomp (my $prefix = <STDIN>);
+
+say "The prfix is : $prefix";
+
+if (defined ($prefix)) {
+    
+    my @files = glob "*.pdf";
+    
+    #say @files;
+    
+    my $year_month_day=strftime("%Y%m%d",localtime());
+    
+    my $handleDir = "${prefix}_${year_month_day}";
+    say "mkdir $handleDir";
+    mkdir $handleDir;
+    
+    foreach my $f (@files) {
+        copy ("$f", "${handleDir}/${prefix}_${f}");
+    }
+}
